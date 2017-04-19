fe820b7c4f7c95d7f404cb610b2e31cc5a05798b
Ying Su <grasssu@gmail.com>
2017-04-18 22:00:15 -0700

    Add new Sudoku file.

---
+++ Sukoku/Sudoku.hpp
@@ -0,0 +1,14 @@
+//
+//  Sudoku.hpp
+//  Sukoku
+//
+//  Created by Ying Su on 4/18/17.
+//  Copyright © 2017 Ying Su. All rights reserved.
+//
+
+#ifndef Sudoku_hpp
+#define Sudoku_hpp
+
+#include <stdio.h>
+
+#endif /* Sudoku_hpp */
